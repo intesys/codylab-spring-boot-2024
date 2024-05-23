30e031d0a58279337c3bef5550b6a0218395f50c
Enrico Costanzi <enrico.costanzi@intesys.it>
2024-05-23 12:53:03 +0200

    all the stateless objects in AppConfiguration

--- src/main/java/it/intesys/academy/config/AppConfiguration.java
+++ src/main/java/it/intesys/academy/config/AppConfiguration.java
@@ -2,11 +2,14 @@ package it.intesys.academy.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import it.intesys.academy.controller.TimeOffController;
+import it.intesys.academy.repository.SQLTimeOffRepository;
+import it.intesys.academy.repository.TimeOffRepository;
+import it.intesys.academy.service.TimeOffService;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 import javax.sql.DataSource;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 public class AppConfiguration {
@@ -38,6 +41,18 @@ public class AppConfiguration {
 
     public static NamedParameterJdbcTemplate jdbcTemplate() {
         return new NamedParameterJdbcTemplate(dataSource());
+    }
+
+    public static TimeOffRepository timeOffRepository() {
+        return new SQLTimeOffRepository(jdbcTemplate());
+    }
+
+    public static TimeOffService timeOffService() {
+        return new TimeOffService(timeOffRepository());
+    }
+
+    public static TimeOffController timeOffController() {
+        return new TimeOffController(timeOffService());
     }
 
 }
