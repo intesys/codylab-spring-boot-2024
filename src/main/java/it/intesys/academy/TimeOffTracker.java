553ad84b6ef05022260c462d4ec9ff1d500c8a53
Talhaimran03 <imrantalha199@gmail.com>
2024-05-24 10:46:52 +0200

    add javalin

--- src/main/java/it/intesys/academy/TimeOffTracker.java
+++ src/main/java/it/intesys/academy/TimeOffTracker.java
@@ -1,5 +1,6 @@
 package it.intesys.academy;
 
+import io.javalin.Javalin;
 import it.intesys.academy.repository.DummyTimeOffRequestRepository;
 import it.intesys.academy.repository.TimeOffRepository;
 import it.intesys.academy.service.TimeOffService;
@@ -10,12 +11,15 @@ import java.time.Duration;
 public class TimeOffTracker {
 
     public static void main(String[] args) {
+        var app = Javalin.create(/*config*/)
+            .get("/", ctx -> ctx.result("Hello World"))
+            .start(8080);
 
-        TimeOffRepository timeOffRepository = new DummyTimeOffRequestRepository();
-        TimeOffService timeOffService = new TimeOffService(timeOffRepository);
-        long userId = 10L;
-
-        Duration timeOffDurationForUser = timeOffService.getTimeOffDurationForUser(userId);
-        System.out.println("User " + userId + " has requested " + StringUtils.format(timeOffDurationForUser) + " time off");
+//        TimeOffRepository timeOffRepository = new DummyTimeOffRequestRepository();
+//        TimeOffService timeOffService = new TimeOffService(timeOffRepository);
+//        long userId = 10L;
+//
+//        Duration timeOffDurationForUser = timeOffService.getTimeOffDurationForUser(userId);
+//        System.out.println("User " + userId + " has requested " + StringUtils.format(timeOffDurationForUser) + " time off");
     }
 }
