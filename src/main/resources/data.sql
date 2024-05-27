802b4302f18c794979f6320d6e6eec0f7eed611e
Enrico Oliosi <enrico.oliosi@intesys.it>
2024-05-27 12:36:14 +0200

    Add simple database structure with data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into yuser (username, name, surname, email) values ( 'eoliosi', 'enrico', 'oliosi', 'enrico.oliosi@intesys.it' );
+
+insert into full_day_timeoff (date_from, date_to, user_id) values ( '2024-01-01', '2024-01-08', 1);
+
+insert into partial_day_timeoff (date, user_id) values ( '2024-01-15', 1);
+
+insert into time_range (time_from, time_to, partial_day_id) values ( '09:00:00', '12:00:00', 1 );
+
+insert into time_range (time_from, time_to, partial_day_id) values ( '17:00:00', '18:00:00', 1 );
--- src/main/resources/yusers.sql
+++
@@ -1,7 +0,0 @@
-create table yuser(
-                      id identity primary key,
-                      available_time_off int
-);
-
-
-insert into yuser(id, available_time_off) values (10, 30)
