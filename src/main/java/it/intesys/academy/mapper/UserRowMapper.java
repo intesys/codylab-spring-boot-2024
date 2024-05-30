f929c06a2aebd19207647b5878d098019731f1a5
Enrico Oliosi <enrico.oliosi@intesys.it>
2024-05-30 16:29:00 +0200

    Simple refactor

--- src/main/java/it/intesys/academy/mapper/UserRowMapper.java
+++ src/main/java/it/intesys/academy/mapper/UserRowMapper.java
@@ -11,15 +11,15 @@ public class UserRowMapper implements RowMapper<User> {
     @Override
     public User mapRow(ResultSet rs, int rowNum) throws SQLException {
 
-        var yUser = new User();
+        var user = new User();
 
-        yUser.setId(rs.getLong("id"));
-        yUser.setName(rs.getString("name"));
-        yUser.setSurname(rs.getString("surname"));
-        yUser.setEmail(rs.getString("email"));
-        yUser.setUsername(rs.getString("username"));
+        user.setId(rs.getLong("id"));
+        user.setName(rs.getString("name"));
+        user.setSurname(rs.getString("surname"));
+        user.setEmail(rs.getString("email"));
+        user.setUsername(rs.getString("username"));
 
-        return yUser;
+        return user;
     }
 
 }
