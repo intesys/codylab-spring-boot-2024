33d1dcf6c1bbf5566b463f74e1a6a1e6ff5a5e66
Enrico Oliosi <enrico.oliosi@intesys.it>
2024-05-30 16:30:08 +0200

    Draft user detail

--- src/main/java/it/intesys/academy/controller/mvc/UserControllerMvc.java
+++ src/main/java/it/intesys/academy/controller/mvc/UserControllerMvc.java
@@ -1,6 +1,8 @@
 package it.intesys.academy.controller.mvc;
 
+import it.intesys.academy.dto.UserDTO;
 import it.intesys.academy.service.UserService;
+import jakarta.websocket.server.PathParam;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,12 +19,23 @@ public class UserControllerMvc {
     }
 
     @GetMapping("/all")
-    public String getIndex(Model model) {
+    public String getUsers(Model model) {
 
         model.addAttribute("users", userService.getAllUsers());
+
+        return "users";
+
+    }
+
+    @GetMapping("/{id}")
+    public String getUser(@PathParam("id") Long id, Model model) {
+
+        model.addAttribute("user", new UserDTO());
 
         return "user";
 
     }
+
+
 
 }
