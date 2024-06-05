95d4bc70db3421a7be72451904f778065d45b407
Enrico Oliosi <enrico.oliosi@intesys.it>
2024-06-05 14:24:25 +0200

    Refactor some component

--- src/main/java/it/intesys/academy/service/UserService.java
+++ src/main/java/it/intesys/academy/service/UserService.java
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 @Service
 public class UserService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
