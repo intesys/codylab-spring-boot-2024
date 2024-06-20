fef6d553bd25074b945763a3d6e638c1b3834e61
Elisabetta Freschi <elisabetta.freschi@intesys.it>
2024-06-20 12:25:33 +0200

    Controller test

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,8 +1,8 @@
-insert into yuser (username, name, surname, email) values ( 'eoliosi', 'enrico', 'oliosi', 'enrico.oliosi@intesys.it' );
+insert into yuser (id, username, name, surname, email) values (1,  'eoliosi', 'enrico', 'oliosi', 'enrico.oliosi@intesys.it' );
 
 insert into full_day_timeoff (date_from, date_to, user_id) values ( '2024-01-01', '2024-01-08', 1);
 
-insert into partial_day_timeoff (date, user_id) values ( '2024-01-15', 1);
+insert into partial_day_timeoff (id, date, user_id) values (9, '2024-01-15', 1);
 
 insert into time_range (time_from, time_to, partial_day_id) values ( '09:00:00', '12:00:00', 9 );
 
---
+++ src/test/java/it/intesys/academy/controller/UserControllerITest.java
@@ -0,0 +1,62 @@
+package it.intesys.academy.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+class UserControllerITest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  @DisplayName("Given a user in DB When get user data THEN response has data")
+  @Sql(scripts = {"/sql/data.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+  @Transactional
+  void whenUserExistsReturnDetail() {
+    try {
+      mockMvc.perform(get("/users/1"))
+          .andExpect(status().isOk())
+          .andExpect(jsonPath("$.id", equalTo(1)))
+          .andExpect(jsonPath("$.username", equalTo("eoliosi")))
+          .andExpect(jsonPath("$.name", equalTo("enrico")))
+          .andExpect(jsonPath("$.surname", equalTo("oliosi")))
+          .andExpect(jsonPath("$.email", equalTo("enrico.oliosi@intesys.it")))
+      ;
+    } catch (Exception e) {
+      fail("No exception is expected here");
+    }
+  }
+
+  @Test
+  @DisplayName("Given a user in DB When get user data THEN response has data")
+  @Sql(scripts = {"/sql/data.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+  @Transactional
+  void whenUserNotExistsThrowsError() {
+    try {
+      mockMvc.perform(get("/users/-2"))
+          .andDo(print())
+          .andExpect(status().is5xxServerError())
+          .andExpect(jsonPath("$.title", equalTo("An unknown error has occurred")))
+      ;
+    } catch (Exception e) {
+      fail("No exception is expected here");
+    }
+  }
+}
