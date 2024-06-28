8a734145cd32bebc870ccce66757bf53f254abf1
Talhaimran03 <imrantalha199@gmail.com>
2024-06-28 19:45:13 +0200

    add openapi.yaml and generator

---
+++ src/main/java/it/intesys/academy/api/TimeOffApiImpl.java
@@ -0,0 +1,19 @@
+package it.intesys.academy.api;
+
+import it.intesys.intesys.academy.api.TimeOffApi;
+import it.intesys.intesys.academy.dto.TimeOffRequestApiDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TimeOffApiImpl implements TimeOffApi {
+
+    @Override
+    public ResponseEntity<TimeOffRequestApiDTO> getTimeOffRequest(String requestId, String user) {
+        return ResponseEntity.ok(
+                new TimeOffRequestApiDTO().esito(TimeOffRequestApiDTO.EsitoEnum.ACCEPTED)
+        );
+    }
+
+
+}
