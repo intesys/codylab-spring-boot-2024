5e25bc3a2a07a98fbe1a48a3840319b382b2c4d4
Enrico Costanzi <enrico.costanzi@intesys.it>
2024-07-05 15:42:36 +0200

    add SecurityUtils

--- src/main/java/it/intesys/academy/api/TimeOffApiImpl.java
+++ src/main/java/it/intesys/academy/api/TimeOffApiImpl.java
@@ -1,13 +1,11 @@
 package it.intesys.academy.api;
 
 import it.intesys.academy.repository.UserRepository;
+import it.intesys.academy.utils.SecurityUtils;
 import it.intesys.intesys.academy.api.TimeOffApi;
 import it.intesys.intesys.academy.dto.TimeOffRequestApiDTO;
 import it.intesys.intesys.academy.dto.TimeOffRequestApiDTO.EsitoEnum;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,9 +19,7 @@ public class TimeOffApiImpl implements TimeOffApi {
 
   @Override
   public ResponseEntity<TimeOffRequestApiDTO> getTimeOffRequest(Long requestId) {
-    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-    User principal = (User) authentication.getPrincipal();
-    String username = principal.getUsername();
+    String username = SecurityUtils.getUsername();
     var user = userRepository.findByUsername(username)
         .orElseThrow();
 
@@ -32,5 +28,6 @@ public class TimeOffApiImpl implements TimeOffApi {
         new TimeOffRequestApiDTO().esito(EsitoEnum.ACCEPTED)
     );
   }
+
 
 }
