db4917120c503b996814c9e594158917ea24435f
Talhaimran03 <imrantalha199@gmail.com>
2024-07-11 12:56:23 +0200

    remove unused imports

--- src/main/java/it/intesys/academy/api/TimeOffApiImpl.java
+++ src/main/java/it/intesys/academy/api/TimeOffApiImpl.java
@@ -7,9 +7,6 @@ import it.intesys.intesys.academy.api.TimeOffApi;
 import it.intesys.intesys.academy.dto.TimeOffRequestApiDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
